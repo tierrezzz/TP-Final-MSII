6a6fa7d06476fa144917fb892b9c905e6210f9cf
Facundo Uriel Gutierrez <tdetierrez@gmail.com>
2025-11-09 20:37:45 -0300

    Base de datos

--- TP-Final.sql
+++ TP-Final.sql
@@ -1,3 +1,5 @@
+CREATE DATABASE  IF NOT EXISTS `trabajo-final-msii` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_spanish_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
+USE `trabajo-final-msii`;
 -- MySQL dump 10.13  Distrib 8.0.43, for Win64 (x86_64)
 --
 -- Host: localhost    Database: trabajo-final-msii
@@ -34,6 +36,16 @@ CREATE TABLE `habitaciones` (
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
+-- Dumping data for table `habitaciones`
+--
+
+LOCK TABLES `habitaciones` WRITE;
+/*!40000 ALTER TABLE `habitaciones` DISABLE KEYS */;
+INSERT INTO `habitaciones` VALUES (1,1,2,4000.00),(2,1,2,4000.00),(5,2,1,2000.00),(6,2,1,2000.00),(8,3,4,6000.00),(9,3,4,6000.00);
+/*!40000 ALTER TABLE `habitaciones` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
 -- Table structure for table `reservas`
 --
 
@@ -51,8 +63,18 @@ CREATE TABLE `reservas` (
   KEY `habitacion_id` (`habitacion_id`),
   CONSTRAINT `reservas_ibfk_1` FOREIGN KEY (`usuario_id`) REFERENCES `usuarios` (`id`),
   CONSTRAINT `reservas_ibfk_2` FOREIGN KEY (`habitacion_id`) REFERENCES `habitaciones` (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_spanish_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_spanish_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `reservas`
+--
+
+LOCK TABLES `reservas` WRITE;
+/*!40000 ALTER TABLE `reservas` DISABLE KEYS */;
+INSERT INTO `reservas` VALUES (1,1,1,'2025-12-20','2025-12-23'),(5,8,1,'2025-11-28','2025-11-30'),(10,8,9,'2025-11-09','2025-11-11');
+/*!40000 ALTER TABLE `reservas` ENABLE KEYS */;
+UNLOCK TABLES;
 
 --
 -- Table structure for table `tipos_habitacion`
@@ -70,6 +92,16 @@ CREATE TABLE `tipos_habitacion` (
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
+-- Dumping data for table `tipos_habitacion`
+--
+
+LOCK TABLES `tipos_habitacion` WRITE;
+/*!40000 ALTER TABLE `tipos_habitacion` DISABLE KEYS */;
+INSERT INTO `tipos_habitacion` VALUES (1,'Matrimonial','Habitacion con cama matrimonial, ideal para parejas. Incluye baño privado, aire acondicionado, television y servicio a la habitacion.'),(2,'Individual','Habitacion comoda para una persona, equipada con cama individual, escritorio, baño privado y conexion Wi-Fi gratuita.'),(3,'Familiar','Amplia habitacion con capacidad para hasta 4 personas, perfecta para familias. Cuenta con dos camas matrimoniales, baño privado y minibar');
+/*!40000 ALTER TABLE `tipos_habitacion` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
 -- Table structure for table `usuarios`
 --
 
@@ -82,8 +114,18 @@ CREATE TABLE `usuarios` (
   `hashed_password` varchar(255) COLLATE utf8mb4_spanish_ci NOT NULL,
   PRIMARY KEY (`id`),
   UNIQUE KEY `username` (`username`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_spanish_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_spanish_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `usuarios`
+--
+
+LOCK TABLES `usuarios` WRITE;
+/*!40000 ALTER TABLE `usuarios` DISABLE KEYS */;
+INSERT INTO `usuarios` VALUES (1,'facundo','$argon2id$v=19$m=65536,t=3,p=4$MmPk6yhKxS6y60YDDcIpZQ$YOtJksAOntkK/zg402Wq8q/Ji0KZMS+mj1Ox6qRr4bQ'),(2,'manuel','$argon2id$v=19$m=65536,t=3,p=4$wjG+CGl46fkZ69dQiE/Z1g$ncTwscvNnHU6ITuXmNLBzOmgMVZ3a/cu8sMOH9HS8BE'),(8,'TTierrez','$argon2id$v=19$m=65536,t=3,p=4$qtParTs+N71sTaVVf3g0SA$EMAL/kKSVJHymqsYh5TUCEp8u3Pop4X2cKIPcQ7dRjU'),(9,'facundo_gutierrez','$argon2id$v=19$m=65536,t=3,p=4$wA5niE4ZW0WoEsINKhPFPQ$DkrpMXxkSxbRrEMRVGznqzlLc8Bl6s/+zKv24BbkMOo');
+/*!40000 ALTER TABLE `usuarios` ENABLE KEYS */;
+UNLOCK TABLES;
 
 --
 -- Dumping routines for database 'trabajo-final-msii'
@@ -113,6 +155,33 @@ DELIMITER ;
 /*!50003 SET character_set_client  = @saved_cs_client */ ;
 /*!50003 SET character_set_results = @saved_cs_results */ ;
 /*!50003 SET collation_connection  = @saved_col_connection */ ;
+/*!50003 DROP PROCEDURE IF EXISTS `sp_reporte_ventas_mensual` */;
+/*!50003 SET @saved_cs_client      = @@character_set_client */ ;
+/*!50003 SET @saved_cs_results     = @@character_set_results */ ;
+/*!50003 SET @saved_col_connection = @@collation_connection */ ;
+/*!50003 SET character_set_client  = utf8mb4 */ ;
+/*!50003 SET character_set_results = utf8mb4 */ ;
+/*!50003 SET collation_connection  = utf8mb4_0900_ai_ci */ ;
+/*!50003 SET @saved_sql_mode       = @@sql_mode */ ;
+/*!50003 SET sql_mode              = 'ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION' */ ;
+DELIMITER ;;
+CREATE DEFINER=`root`@`localhost` PROCEDURE `sp_reporte_ventas_mensual`()
+BEGIN
+    SELECT
+        YEAR(r.fecha_inicio) AS anio,
+        MONTH(r.fecha_inicio) AS mes,
+        -- Eliminamos la columna 'semana'
+        SUM(DATEDIFF(r.fecha_fin, r.fecha_inicio) * h.precio) AS total_ventas
+    FROM reservas r
+    JOIN habitaciones h ON r.habitacion_id = h.id
+    GROUP BY anio, mes
+    ORDER BY anio, mes;
+END ;;
+DELIMITER ;
+/*!50003 SET sql_mode              = @saved_sql_mode */ ;
+/*!50003 SET character_set_client  = @saved_cs_client */ ;
+/*!50003 SET character_set_results = @saved_cs_results */ ;
+/*!50003 SET collation_connection  = @saved_col_connection */ ;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
@@ -123,4 +192,4 @@ DELIMITER ;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2025-11-04 19:24:06
+-- Dump completed on 2025-11-09 20:36:46
